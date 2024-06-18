f7cfce510e37e043846fe1d473767ff4c2308db7
plusdojo2024-satoGai <satou-gai-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:44:45 +0900

    Update RecordViewServlet.java

--- src/servlet/RecordViewServlet.java
+++ src/servlet/RecordViewServlet.java
@@ -1,4 +1,22 @@
 package servlet;
-public class RecordViewServlet {
-    
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RecordViewServlet")
+public class RecordViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/view_board_record.jsp");
+		dispatcher.forward(request, response);
+	}
+
 }
