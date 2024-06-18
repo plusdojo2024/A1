20bc8081d147101421dfbfcfbf8b2ab1450a820b
plusdojo2024-akazawa <akazawa-nonoha-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:20:37 +0900

    Servletの修正

--- src/servlet/AllMarkResultServlet.java
+++ src/servlet/AllMarkResultServlet.java
@@ -1,4 +1,29 @@
 package servlet;
-public class AllMarkResultServlet {
-    
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/AllMarkResultServlet")
+public class AllMarkResultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/all_marker_record.jsp");
+		dispatcher.forward(request, response);
+	}
+
 }
