89cfa14e7cd8d70a511caf7514abcbab266b3fd3
plusdojo2024-akiyama <akiyama-hibiki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 10:37:06 +0900

    Servlet

--- src/servlet/AllComServlet.java
+++ src/servlet/AllComServlet.java
@@ -1,4 +1,41 @@
 package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.AllComDAO;
+import model.AllCom;
+
+
 public class AllComServlet {
-    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String allComContents = request.getParameter("allComContents");
+
+
+
+
+		// 登録処理を行う
+		AllComDAO acDao = new AllComDAO();
+		if (acDao.insert(new AllCom(0,allComContents,null))) {
+		}
+		else {
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
+		dispatcher.forward(request, response);
+	}
 }
+
+
