7dd443a1a765ac5d925063e0b1de080d3bad2fb0
plusdojo2024-ito <ito-yousei-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 17:55:42 +0900

    jsp追加

--- src/servlet/RecordServlet.java
+++ src/servlet/RecordServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BlackBoardDAO;
+import model.BlackBoard;
+
 /**
  * Servlet implementation class LoginServlet
  */
@@ -16,10 +20,17 @@ import javax.servlet.http.HttpServletResponse;
 public class RecordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    BlackBoardDAO dao = new BlackBoardDAO();
+    List<BlackBoard> list = dao.select();
+    request.setAttribute("blackBoardList", list);
+
+
 		// ログインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/t_board_record.jsp");
 		dispatcher.forward(request, response);
