24448ecb84a13e6ed9db888ba5197dded35ea11a
plusdojo2024-satoGai <satou-gai-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 10:53:04 +0900

    Update BlackBoardDAO.java
    出来ているか不安なので確認していただけるとありがたいです。

--- src/dao/BlackBoardDAO.java
+++ src/dao/BlackBoardDAO.java
@@ -25,7 +25,7 @@ public class BlackBoardDAO {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
 
 			// SQL文を準備する
-			String sql = "SELECT * FROM black_board";
+			String sql = "SELECT * FROM black_board ";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を実行し、結果表を取得する
@@ -66,7 +66,7 @@ public class BlackBoardDAO {
 		return blackBoardList;
 	}
 
-public boolean insert(BlackBoard blackBoard) {
+	public boolean insert(BlackBoard blackBoard) {
 	Connection conn = null;
 	boolean result = false;
 
@@ -111,4 +111,148 @@ public boolean insert(BlackBoard blackBoard) {
 	// 結果を返す
 	return result;
 }
+
+
+//講師による板書への加筆を行えるようにする。
+public boolean update(BlackBoard blackBoard) {
+	Connection conn = null;
+	boolean result = false;
+
+	try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+		// SQL文を準備する
+
+		String sql = "UPDATE black_board SET board_contents = ? "
+				+ "WHERE board_id = (SELECT MAX(board_id) FROM black_board);";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SQL文を完成させる
+		pStmt.setString(0 , blackBoard.getBoardContents());
+
+		// SQL文を実行する
+		if (pStmt.executeUpdate() == 1) {
+			result = true;
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
+}
+
+
+//板書履歴に日付一覧を表示するために、今までの日付を持ってくる。
+public boolean selectDate(BlackBoard blackBoard) {
+	Connection conn = null;
+	boolean result = false;
+
+	try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+		// SQL文を準備する
+
+		String sql = "select board_date from black_board order by board_date desc;";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SQL文を実行する
+		if (pStmt.executeUpdate() == 1) {
+			result = true;
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
+}
+
+
+//板書履歴の日付をクリックした際に、過去板書の中身を持ってくる
+public boolean selectBoard(BlackBoard blackBoard) {
+	Connection conn = null;
+	boolean result = false;
+
+	try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+		// SQL文を準備する
+
+		String sql = "select board_date, board_contents from black_board where board_date = ?;";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SQL文を完成させる
+		pStmt.setDate(0 , blackBoard.getBlackBoardDatetime());
+
+		// SQL文を実行する
+		if (pStmt.executeUpdate() == 1) {
+			result = true;
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
+}
+
+
 }
