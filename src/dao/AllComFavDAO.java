b9f67fc794852b67d4d5cf888c2e5b4c6d180699
plusdojo2024-akiyama <akiyama-hibiki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 10:55:31 +0900

    favにdelite追加

--- src/dao/AllComFavDAO.java
+++ src/dao/AllComFavDAO.java
@@ -158,4 +158,52 @@ public class AllComFavDAO {
 		return count;
 
 	}
+
+
+public boolean delete(int number) {
+	Connection conn = null;
+	boolean result = false;
+
+	try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+		// SQL文を準備する
+		String sql = "DELETE FROM marker_com_fav WHERE all_com_fav_num=?";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SQL文を完成させる
+		pStmt.setInt(1, number);
+
+		// SQL文を実行する
+		if (pStmt.executeUpdate() == 1) {
+			result = true;
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
 }
+}
+
+
