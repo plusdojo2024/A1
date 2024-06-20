3e6d939d362f2608fa2218a4da99bdef80965e6c
plusdojo2024-satoGai <satou-gai-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 12:05:21 +0900

    MarkerRecDAOの引数修正

--- src/dao/MarkerRecDAO.java
+++ src/dao/MarkerRecDAO.java
@@ -11,11 +11,428 @@ import java.util.List;
 import model.MarkerRec;
 
 public class MarkerRecDAO {
-//-------------()
+
+//-------------(理解度を登録する)----------------------------------------------
+//よくわかるを登録する
+	public boolean insertVg(int UserId , int MarkerId) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+			// SQL文を準備する
+			String sql = "INSERT INTO marker_rec(user_id, marker_id, "
+					+ "flag_very_good, flag_good, flag_bad, flag_very_bad) "
+					+ "VALUES (?, ?, 1, 0, 0, 0);";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, UserId);
+			pStmt.setInt(2, MarkerId);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// 結果を返す
+		return result;
+	}
+
+//わからないを登録する
+		public boolean insertB(int UserId , int MarkerId) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+				// SQL文を準備する
+				String sql = "INSERT INTO marker_rec(user_id, marker_id, "
+						+ "flag_very_good, flag_good, flag_bad, flag_very_bad) "
+						+ "VALUES (?, ?, 0, 0, 1, 0);";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, UserId);
+				pStmt.setInt(2, MarkerId);
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+
+//全くわからない を登録する
+		public boolean insertVb(int UserId , int MarkerId) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+				// SQL文を準備する
+				String sql = "INSERT INTO marker_rec(user_id, marker_id, "
+						+ "flag_very_good, flag_good, flag_bad, flag_very_bad) "
+						+ "VALUES (?, ?, 0, 0, 0, 1);";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, UserId);
+				pStmt.setInt(2, MarkerId);
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
+//------------------理解度を再登録する。---------------------------------------------------
+//update 理解度の再登録　よくわかる
+		public List<MarkerRec> updateVg(int UserId , int MarkerId) {
+			Connection conn = null;
+			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+				// SQL文を準備する
+				String sql = "UPDATE marker_rec SET"
+						+ " flag_very_good = 1, flag_good = 0, flag_bad = 0, flag_very_bad = 0 "
+						+ "WHERE Uuser_id = ? AND marker_id = ?;";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, UserId);
+				pStmt.setInt(2, MarkerId);
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					MarkerRec record = new MarkerRec(
+					rs.getInt("markerRecNumber"),
+					rs.getInt("userId"),
+					rs.getInt("markerId"),
+					rs.getInt("flagVeryGood"),
+					rs.getInt("flagGood"),
+					rs.getInt("flagBad"),
+					rs.getInt("flagVeryBad"),
+					rs.getDate("markerRecDatetime")
+					);
+					markerRecList.add(record);
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				markerRecList = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				markerRecList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return markerRecList;
+		}
+
+//理解度の再登録　分かる
+		public List<MarkerRec> updateG(int UserId , int MarkerId) {
+			Connection conn = null;
+			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+				// SQL文を準備する
+				String sql = "UPDATE marker_rec SET"
+						+ " flag_very_good = 0, flag_good = 1, flag_bad = 0, flag_very_bad = 0 "
+						+ "WHERE User_id = ? AND marker_id = ?;";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, UserId);
+				pStmt.setInt(2, MarkerId);
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					MarkerRec record = new MarkerRec(
+					rs.getInt("markerRecNumber"),
+					rs.getInt("userId"),
+					rs.getInt("markerId"),
+					rs.getInt("flagVeryGood"),
+					rs.getInt("flagGood"),
+					rs.getInt("flagBad"),
+					rs.getInt("flagVeryBad"),
+					rs.getDate("markerRecDatetime")
+					);
+					markerRecList.add(record);
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				markerRecList = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				markerRecList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return markerRecList;
+		}
+
+//理解度の再登録　分からない
+				public List<MarkerRec> updateB(int UserId , int MarkerId) {
+					Connection conn = null;
+					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
+
+					try {
+						// JDBCドライバを読み込む
+						Class.forName("org.h2.Driver");
+
+						// データベースに接続する
+						conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+						// SQL文を準備する
+						String sql = "UPDATE marker_rec SET"
+								+ " flag_very_good = 0, flag_good = 0, flag_bad = 1, flag_very_bad = 0 "
+								+ "WHERE User_id = ? AND marker_id = ?;";
+
+						PreparedStatement pStmt = conn.prepareStatement(sql);
+
+						// SQL文を完成させる
+						pStmt.setInt(1, UserId);
+						pStmt.setInt(2, MarkerId);
+
+						// SQL文を実行し、結果表を取得する
+						ResultSet rs = pStmt.executeQuery();
+
+						// 結果表をコレクションにコピーする
+						while (rs.next()) {
+							MarkerRec record = new MarkerRec(
+							rs.getInt("markerRecNumber"),
+							rs.getInt("userId"),
+							rs.getInt("markerId"),
+							rs.getInt("flagVeryGood"),
+							rs.getInt("flagGood"),
+							rs.getInt("flagBad"),
+							rs.getInt("flagVeryBad"),
+							rs.getDate("markerRecDatetime")
+							);
+							markerRecList.add(record);
+						}
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+								markerRecList = null;
+							}
+						}
+					}
+
+					// 結果を返す
+					return markerRecList;
+				}
+
+
+//理解度の再登録　分からない
+				public List<MarkerRec> updateVb(int UserId , int MarkerId) {
+					Connection conn = null;
+					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
+
+					try {
+						// JDBCドライバを読み込む
+						Class.forName("org.h2.Driver");
+
+						// データベースに接続する
+						conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
+
+						// SQL文を準備する
+						String sql = "UPDATE marker_rec SET"
+								+ " flag_very_good = 0, flag_good = 0, flag_bad = 0, flag_very_bad = 1 "
+								+ "WHERE User_id = ? AND marker_id = ?;";
+
+						PreparedStatement pStmt = conn.prepareStatement(sql);
+
+						// SQL文を完成させる
+						pStmt.setInt(1,  UserId);
+						pStmt.setInt(2, MarkerId);
+
+						// SQL文を実行し、結果表を取得する
+						ResultSet rs = pStmt.executeQuery();
+
+						// 結果表をコレクションにコピーする
+						while (rs.next()) {
+							MarkerRec record = new MarkerRec(
+							rs.getInt("markerRecNumber"),
+							rs.getInt("userId"),
+							rs.getInt("markerId"),
+							rs.getInt("flagVeryGood"),
+							rs.getInt("flagGood"),
+							rs.getInt("flagBad"),
+							rs.getInt("flagVeryBad"),
+							rs.getDate("markerRecDatetime")
+							);
+							markerRecList.add(record);
+						}
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+						markerRecList = null;
+					}
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+								markerRecList = null;
+							}
+						}
+					}
+
+					// 結果を返す
+					return markerRecList;
+				}
+
 
 
 //ーーーーーーーーーーーー受講生_理解度履歴ページ 理解度の円グラフーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-	public List<MarkerRec> markerPieChart(MarkerRec markerRec) {
+	public List<MarkerRec> markerPieChart(int UserId) {
 		Connection conn = null;
 		List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -27,7 +444,7 @@ public class MarkerRecDAO {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A1", "sa", "");
 
 			// SQL文を準備する
-			String sql = "sSELECT users.user_name, marker.marker_contents, "
+			String sql = "SELECT users.user_name, marker.marker_contents, "
 					+ "COUNT(marker_rec.flag_very_good) AS very_good, COUNT(marker_rec.flag_good) "
 					+ "AS good, COUNT(marker_rec.flag_bad) AS bad, COUNT(marker_rec.flag_very_bad) "
 					+ "AS very_bad FROM users INNER JOIN marker_rec ON users.user_id = lmarker_rec.user_id "
@@ -36,7 +453,7 @@ public class MarkerRecDAO {
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			pStmt.setInt(1, markerRec.getUserId());
+			pStmt.setInt(1, UserId );
 
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
@@ -88,7 +505,7 @@ public class MarkerRecDAO {
 
 
 	//マーカー項目に全体のうち何人が（よくわかる）を押したか数える。
-	public List<MarkerRec> countAllVg(MarkerRec markerRec) {
+	public List<MarkerRec> countAllVg(int MarkerId) {
 		Connection conn = null;
 		List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -109,7 +526,7 @@ public class MarkerRecDAO {
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			pStmt.setInt(1, markerRec.getMarkerId());
+			pStmt.setInt(1, MarkerId );
 
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();
@@ -155,7 +572,7 @@ public class MarkerRecDAO {
 	}
 
 //マーカー項目に全体のうち何人が（わかる）を押したか数える。
-		public List<MarkerRec> countAllG(MarkerRec markerRec) {
+		public List<MarkerRec> countAllG(int MarkerId) {
 			Connection conn = null;
 			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -176,7 +593,7 @@ public class MarkerRecDAO {
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, markerRec.getMarkerId());
+				pStmt.setInt(1,MarkerId);
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
@@ -222,7 +639,7 @@ public class MarkerRecDAO {
 		}
 
 //マーカー項目に全体のうち何人が（分からない）を押したか数える。
-		public List<MarkerRec> countAllB(MarkerRec markerRec) {
+		public List<MarkerRec> countAllB(int MarkerId) {
 			Connection conn = null;
 			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -243,7 +660,7 @@ public class MarkerRecDAO {
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, markerRec.getMarkerId());
+				pStmt.setInt(1, MarkerId);
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
@@ -289,7 +706,7 @@ public class MarkerRecDAO {
 		}
 
 //マーカー項目に全体のうち何人が（全く分からない）を押したか数える。
-		public List<MarkerRec> countAllVb(MarkerRec markerRec) {
+		public List<MarkerRec> countAllVb(int MarkerId) {
 			Connection conn = null;
 			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -310,7 +727,7 @@ public class MarkerRecDAO {
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, markerRec.getMarkerId());
+				pStmt.setInt(1,MarkerId);
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
@@ -357,7 +774,7 @@ public class MarkerRecDAO {
 
 //-------------------(個数判別)--------------------------------------------------------------------------------------
 //個人の理解度の個数判別(very_good)
-		public List<MarkerRec> countVg(MarkerRec markerRec) {
+		public List<MarkerRec> countVg(int UserId) {
 			Connection conn = null;
 			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -378,7 +795,7 @@ public class MarkerRecDAO {
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, markerRec.getUserId());
+				pStmt.setInt(1, UserId);
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
@@ -424,7 +841,7 @@ public class MarkerRecDAO {
 		}
 
 //個人の理解度の個数判別(good)
-				public List<MarkerRec> countG(MarkerRec markerRec) {
+				public List<MarkerRec> countG(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -445,7 +862,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1, UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
@@ -491,7 +908,7 @@ public class MarkerRecDAO {
 				}
 
 //個人の理解度の個数判別(bad)
-				public List<MarkerRec> countB(MarkerRec markerRec) {
+				public List<MarkerRec> countB(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -512,7 +929,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1,UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
@@ -558,7 +975,7 @@ public class MarkerRecDAO {
 				}
 
 //個人の理解度の個数判別(very_good)
-				public List<MarkerRec> countVb(MarkerRec markerRec) {
+				public List<MarkerRec> countVb(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -579,7 +996,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1, UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
@@ -630,7 +1047,7 @@ public class MarkerRecDAO {
 
 //-------------------(個人理解度の一覧表示)--------------------------------------------------------------------------------------
 //個人理解度の一覧表示(very_good)
-		public List<MarkerRec> selectVg(MarkerRec markerRec) {
+		public List<MarkerRec> selectVg(int UserId) {
 			Connection conn = null;
 			List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -651,7 +1068,7 @@ public class MarkerRecDAO {
 				PreparedStatement pStmt = conn.prepareStatement(sql);
 
 				// SQL文を完成させる
-				pStmt.setInt(1, markerRec.getUserId());
+				pStmt.setInt(1,UserId);
 
 				// SQL文を実行し、結果表を取得する
 				ResultSet rs = pStmt.executeQuery();
@@ -697,7 +1114,7 @@ public class MarkerRecDAO {
 		}
 
 		//個人理解度の一覧表示(good)
-				public List<MarkerRec> selectG(MarkerRec markerRec) {
+				public List<MarkerRec> selectG(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -718,7 +1135,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1,UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
@@ -764,7 +1181,7 @@ public class MarkerRecDAO {
 				}
 
 				//個人理解度の一覧表示(bad)
-				public List<MarkerRec> selectB(MarkerRec markerRec) {
+				public List<MarkerRec> selectB(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -785,7 +1202,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1,UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
@@ -831,7 +1248,7 @@ public class MarkerRecDAO {
 				}
 
 				//個人理解度の一覧表示(very_bad)
-				public List<MarkerRec> selectVb(MarkerRec markerRec) {
+				public List<MarkerRec> selectVb(int UserId) {
 					Connection conn = null;
 					List<MarkerRec> markerRecList = new ArrayList<MarkerRec>();
 
@@ -852,7 +1269,7 @@ public class MarkerRecDAO {
 						PreparedStatement pStmt = conn.prepareStatement(sql);
 
 						// SQL文を完成させる
-						pStmt.setInt(1, markerRec.getUserId());
+						pStmt.setInt(1, UserId);
 
 						// SQL文を実行し、結果表を取得する
 						ResultSet rs = pStmt.executeQuery();
