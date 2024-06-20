4a57a9b6c707780ade130a2c9bae37a677fc6372
plusdojo2024-ito <ito-yousei-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 11:42:57 +0900

    Record追加

--- src/servlet/RecordViewServlet.java
+++ src/servlet/RecordViewServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,19 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BlackBoardDAO;
+import model.BlackBoard;
+
 @WebServlet("/RecordViewServlet")
 public class RecordViewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-<<<<<<< HEAD
+
 		BlackBoardDAO dao = new BlackBoardDAO();
 	    List<BlackBoard> list = dao.select();
 	    request.setAttribute("blackBoardList", list);
 
-=======
->>>>>>> ee09b77135decc1287b3967def6411815f11762d
+
 		// ログインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/view_board_record.jsp");
 		dispatcher.forward(request, response);
